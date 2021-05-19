deace087ed2ab1597786e6fc9b2ed4b16b5d9d39
Caleb Wagner <calebwagner5@gmail.com>
2021-05-19 15:23:35 -0500

    added fetch calls and getters

--- src/scripts/data/provider.js
+++ src/scripts/data/provider.js
@@ -1,12 +1,80 @@
-const apiURL = "http://localhost:8088"
-const applicationElement = document.querySelector(".giffygram")
-
+const apiURL = "http://localhost:8088";
+const applicationElement = document.querySelector(".giffygram");
 
 const applicationState = {
-    currentUser: {},
-    feed: {
-        chosenUser: null,
-        displayFavorites: false,
-        displayMessages: false
-    }
-}
+  currentUser: {},
+  feed: {
+    chosenUser: null,
+    displayFavorites: false,
+    displayMessages: false,
+  },
+  users: [],
+  posts: [],
+  likes: [],
+  messages: [],
+};
+
+const mainContainer = document.querySelector(".giffygram");
+
+export const favoritePost = (likedPost) => {
+  const fetchOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(likedPost),
+  };
+  return fetch(`${apiURL}/posts`, fetchOptions)
+    .then((response) => response.json())
+    .then(() => {
+      mainContainer.dispatchEvent(new CustomEvent("stateChanged"));
+    });
+};
+
+export const fetchUsers = () => {
+  return fetch(`${apiURL}/users`)
+    .then((response) => response.json())
+    .then((user) => {
+      applicationState.users = user;
+    });
+};
+
+export const fetchPosts = () => {
+  return fetch(`${apiURL}/posts`)
+    .then((response) => response.json())
+    .then((post) => {
+      applicationState.post = post;
+    });
+};
+
+export const fetchLikes = () => {
+  return fetch(`${apiURL}/likes`)
+    .then((response) => response.json())
+    .then((like) => {
+      applicationState.likes = like;
+    });
+};
+
+export const fetchMessages = () => {
+  return fetch(`${apiURL}/messages`)
+    .then((response) => response.json())
+    .then((message) => {
+      applicationState.messages = message;
+    });
+};
+
+export const getUsers = () => {
+  return [...applicationState.users];
+};
+
+export const getPosts = () => {
+  return [...applicationState.posts];
+};
+
+export const getLikes = () => {
+  return [...applicationState.likes];
+};
+
+export const getMessages = () => {
+  return [...applicationState.messages];
+};
