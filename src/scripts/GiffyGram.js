650fb6a0343e237295412e140ac8ba115996b2d7
Andrew Webb <webba@ihopu.org>
2021-05-19 15:24:43 -0500

    created nav,div, and footer html tags

--- src/scripts/GiffyGram.js
+++ src/scripts/GiffyGram.js
@@ -1,5 +1,17 @@
+
+
 export const GiffyGram = () => {
 
     // Show main main UI
-    return `<h1>Giffygram</h1>`
+    return `<h1>Giffygram</h1>
+    <nav class="navigation"></nav>
+
+    <div class="giffygram__feed">
+    
+    </div>
+
+    <footer class="footer">
+    
+    </footer>
+    `
 }
