9dd59b7d73ca28056d6c238c6d7062d6734ab843
Andrew Webb <webba@ihopu.org>
2021-05-19 16:06:26 -0500

    working on login

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,27 @@
-import { GiffyGram } from "./GiffyGram.js"
-import { LoginForm } from "./auth/Login.js"
+import { GiffyGram } from "./GiffyGram.js";
+import { LoginForm } from "./auth/Login.js";
+import { fetchUsers } from "./data/provider.js";
 
-const applicationElement = document.querySelector(".giffygram")
+const applicationElement = document.querySelector(".giffygram");
 
 export const renderApp = () => {
-    const user = parseInt(localStorage.getItem("gg_user"))
+  const user = parseInt(localStorage.getItem("gg_user"));
 
+  fetchUsers()
+  .then(
+      () => {
     if (user) {
-        applicationElement.innerHTML = GiffyGram()
+      applicationElement.innerHTML = GiffyGram();
     } else {
-        applicationElement.innerHTML = LoginForm()
+      applicationElement.innerHTML = LoginForm();
     }
-}
+  });
+};
 
 renderApp();
+
+applicationElement.addEventListener(
+    "stateChanged", 
+    (customEvent) => {
+    renderApp();
+});
