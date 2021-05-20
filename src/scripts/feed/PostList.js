224c9b40ea5bb303f99f27c8d547dea41f46d48e
Caleb Wagner <calebwagner5@gmail.com>
2021-05-20 13:10:11 -0500

    create posts list

--- src/scripts/feed/PostList.js
+++ src/scripts/feed/PostList.js
@@ -4,24 +4,28 @@ export const PostList = () => {
   const posts = getPosts();
   const likes = getLikes();
   let html = `
-  <div class="">
+  <section class="post">
   ${posts
     .map((post) => {
       return `
-      <section class="post">
-      <header>
-      <h2 class="post__title">${post.title}</h2>
-      </header>
-      <img class="post__image" src="${post.imageURL}">
-      <div class="post__description">${post.description}</div>
-      <div class="post__tagline">Posted by 
-      <a href="#" class="profileLink" id="profile--"</a>
-      on ${post.timestamp}
-      </div>
+        <header>
+            <h2 class="post__title">${post.title}</h2>
+        </header>
+        <img class="post__image" src="${post.imageURL}">
+        <div class="post__description">${post.description}</div>
+        <div class="post__tagline">Posted by 
+            <a href="#" class="profileLink" id="profile--"</a>
+            on ${post.timestamp}
+        </div>
+        <div>
+            <div>
+                <img id="favoritePost--4" class="actionIcon" src="/images/favorite-star-blank.svg">
+            </div>
+        </div>
       `;
     })
     .join("")}
-  </div>
+  </section>
   `;
   return html;
 };
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,34 +1,30 @@
 import { GiffyGram } from "./GiffyGram.js";
 import { LoginForm } from "./auth/Login.js";
-import { fetchUsers } from "./data/provider.js";
-import { Register } from "./auth/Register.js"
+import { fetchPosts, fetchUsers } from "./data/provider.js";
+import { Register } from "./auth/Register.js";
 
 const applicationElement = document.querySelector(".giffygram");
 
 export const renderApp = () => {
   const user = parseInt(localStorage.getItem("gg_user"));
 
-  const newUser = localStorage.getItem("gg_newUser")
+  const newUser = localStorage.getItem("gg_newUser");
 
   fetchUsers()
-  .then(
-      () => {
-    if (user) {
-      applicationElement.innerHTML = GiffyGram();
-    } 
-    
-    else if (newUser) {
-        applicationElement.innerHTML = Register()
-    } else {
-      applicationElement.innerHTML = LoginForm();
-    }
-  });
+    .then(fetchPosts)
+    .then(() => {
+      if (user) {
+        applicationElement.innerHTML = GiffyGram();
+      } else if (newUser) {
+        applicationElement.innerHTML = Register();
+      } else {
+        applicationElement.innerHTML = LoginForm();
+      }
+    });
 };
 
 renderApp();
 
-applicationElement.addEventListener(
-    "stateChanged", 
-    (customEvent) => {
-    renderApp();
+applicationElement.addEventListener("stateChanged", (customEvent) => {
+  renderApp();
 });
